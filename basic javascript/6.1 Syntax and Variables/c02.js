cb72ba3c676bfeca1c9f7973e45fc13789095733
c212 <40395648+c212@users.noreply.github.com>
2019-04-01 08:24:46 -0400

    minor update
    two stages lost this is an update

--- basic javascript/6.1 Syntax and Variables/c02.js
+++ basic javascript/6.1 Syntax and Variables/c02.js
@@ -1,1 +1,5 @@
-va
+var numTickets = 10; // Code tested with values: 10 and 33
+
+
+
+console.log("There are " + numTickets + " tickets.");
--- basic javascript/6.1 Syntax and Variables/c03.js
+++ basic javascript/6.1 Syntax and Variables/c03.js
@@ -1,1 +1,5 @@
-va
+var cityName = prompt('What city do you live in?');
+
+
+
+console.log('Winter in ' + cityName + ' is cool.');
